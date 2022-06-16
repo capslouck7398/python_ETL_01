f046149135112b5d84711b7eff43684baaf40672
will <will31415926535@gmail.com>
2022-06-16 12:53:14 +0800

    homework_01_02

--- 123.py
+++ 123.py
@@ -3,7 +3,10 @@ import json , time , firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate('/home/will/homework/serviceAccount.json')
+with open('ETL_01.txt','r') as f:
+  apiurl = f.read()
+
+cred = credentials.Certificate('serviceAccount.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
@@ -12,7 +15,7 @@ for i in url_array:
   nowTime = int(time.time())
   struct_time = time.localtime(nowTime)
   timeString = time.strftime("%Y-%m-%d-%I:%M:%S-%P", struct_time)
-  respone = json.loads(ur.urlopen("https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-"+i+"?Authorization=CWB-1E99214B-DD88-4ECE-B511-58EC79456EE0").read().decode('utf-8'))
+  respone = json.loads(ur.urlopen("https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-"+i+"?Authorization="+apiurl).read().decode('utf-8'))
   doc_ref = db.collection("homework").document("student01").collection("test02")
   doc_ref.document('weather-'+i+"-"+timeString).set({'weather':respone})
 
